543aba1f186025b5d2128d65240deb0667113d3c
Juanjo Alvarez <juanjo@sourced.tech>
2018-07-30 19:38:47 +0200

    equals_notnull.py
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- python/equals_notnull.py
+++ python/equals_notnull.py
@@ -6,3 +6,14 @@ if __name__ == '__main__':
     client = bblfsh.BblfshClient("0.0.0.0:9432")
 
     uast = client.parse("../java/equals_notnull.java").uast
+    eq_methods = bblfsh.filter(uast, "//MethodDeclaration/Identifier[@Name='equals']/parent::MethodDeclaration")
+
+    for m in eq_methods:
+        for c in m.children:
+            if c.properties["internalRole"] == 'parameters':
+                ann = bblfsh.filter(c, '//MarkerAnnotation//Identifier')
+                ann_qualified = '.'.join([i.properties["Name"] for i in ann])
+
+                if ann_qualified == "javax.annotation.Nonnull":
+                    print("Don't use Nonnull annotation on equals methods")
+                print(ann_qualified)
