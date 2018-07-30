dab539b2251e1d78db8270dcdd2af40627e1c35e
Juanjo Alvarez <juanjo@sourced.tech>
2018-07-30 15:58:10 +0200

    Second test
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- python/avoid_clone.py
+++ python/avoid_clone.py
@@ -2,10 +2,12 @@ import utils
 
 import bblfsh
 
-client = bblfsh.BblfshClient("0.0.0.0:9432")
-uast = client.parse("../java/avoid_clone.java").uast
-methods = bblfsh.filter(uast, "//TypeDeclaration//FunctionGroup")
-for method_node in methods:
-    m = utils.Method(method_node)
-    if m.name == "clone" and "public" in m.modifiers and m.return_.type_name == "Object":
-        print("Don't use clone")
+if __name__ == '__main__':
+    client = bblfsh.BblfshClient("0.0.0.0:9432")
+    uast = client.parse("../java/avoid_clone.java").uast
+    methods = bblfsh.filter(uast, "//TypeDeclaration//FunctionGroup")
+
+    for method_node in methods:
+        m = utils.Method(method_node)
+        if m.name == "clone" and "public" in m.modifiers and m.return_.type_name == "Object":
+            print("Don't use clone")
---
+++ python/binary_instead_boolean.py
@@ -0,0 +1,12 @@
+import utils
+
+import bblfsh
+
+if __name__ == '__main__':
+    client = bblfsh.BblfshClient("0.0.0.0:9432")
+    uast = client.parse("../java/binary_instead_boolean.java").uast
+    ifs = bblfsh.filter(uast, "//IfStatement/*[@internalRole = 'expression' "
+                              "and @roleBitwise and @roleCondition]")
+
+    if len(list(ifs)) > 0:
+        print("Potential bug: bitwise operator inside if condition")
