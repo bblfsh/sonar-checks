24a15b88997e00d8fc4c75214c9de6f2ee758cd7
Juanjo Alvarez <juanjo@sourced.tech>
2018-07-30 21:03:16 +0200

    Added for_loopvar_modify.py
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- java/for_loopvar_modify.java
+++ java/for_loopvar_modify.java
@@ -1,7 +1,10 @@
 class ForLoopVarModify {
     void test() {
-        for (i = 0; i < 10; j++) {
+        for (i = 0; i < 10; i++) {
+            --i;
             i++;
+            i+=1;
+            i=10;
         }
     }
 }
