ff7338b8227344e1dcedd4989d42f56cfd788253
Juanjo Alvarez <juanjo@sourced.tech>
2018-08-16 18:46:11 +0200

    Added skeleton empty tests
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

---
+++ tests/test.py
@@ -0,0 +1,8 @@
+import unittest
+
+# FIXME XXX: Write the tests:
+"""
+Test should do at a minimum:
+    - Run each check with its default java file and check that there are results.
+    - Run each check with all other others java files and check that it doesn't crash.
+"""
